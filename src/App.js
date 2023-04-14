0942fa58a54217ec95075cc9032308a0e21f4651
kellyg418 <121138635+kellyg418@users.noreply.github.com>
2023-04-13 23:46:35 -0700

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Kelly Granados and is open-sourced on{" "}
+        <a href="https://github.com/kellyg418/react-weather-app">GitHub</a>
+      </footer>
     </div>
   );
 }
