62280b67a04f7c138341faf6bfadb329a044c963
kellyg418 <121138635+kellyg418@users.noreply.github.com>
2023-04-14 23:17:18 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Kelly Granados and is open-sourced on{" "}
-        <a href="https://github.com/kellyg418/react-weather-app">GitHub</a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Kelly Granados and is open-sourced on{" "}
+          <a href="https://github.com/kellyg418/react-weather-app">GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
