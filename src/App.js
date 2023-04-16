6c39f722eebbc7bc6fe36d762c16455cd24c8f33
kellyg418 <121138635+kellyg418@users.noreply.github.com>
2023-04-16 00:43:53 -0700

    Installed axios and API integration
    x

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by Kelly Granados and is open-sourced on{" "}
           <a href="https://github.com/kellyg418/react-weather-app">GitHub</a>
