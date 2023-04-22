fbe84ceb8cb41df109c6272f6ad1e6f9fbd0ac46
kellyg418 <121138635+kellyg418@users.noreply.github.com>
2023-04-22 16:15:22 -0700

    Created a single day forecast

--- src/Weather.js
+++ src/Weather.js
@@ -61,7 +61,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast coordinates={weatherData.coord} />
       </div>
     );
   } else {
